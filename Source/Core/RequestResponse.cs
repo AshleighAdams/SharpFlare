93bbe81d1e2d5e7484e26f5efc675d2f7e033506
Ashleigh Adams <ashleigh.k.adams@gmail.com>
2016-10-12 19:51:18 +0100

    HTTP interfaces work

---
+++ Source/Core/RequestResponse.cs
@@ -0,0 +1,38 @@
+// https://http2.github.io/http2-spec/#HttpRequest
+// http://http2.github.io/http2-spec/compression.html
+using System;
+using System.Collections.Generic;
+using System.Net.Sockets;
+using System.IO;
+
+namespace SharpFlare
+{
+	namespace Http
+	{
+		public enum ResponseCode : uint
+		{
+			Okay = 200,
+			FileNotFound = 404
+		}
+		
+		public interface Request
+		{
+			string Protocol { get; }
+			string Method { get; }
+			string Path { get; }
+			string Authority { get; }
+			string Scheme { get; }
+			Dictionary<string, string> Headers { get; }
+			Stream Content { get; }
+		}
+		
+		public interface Response
+		{
+			ResponseCode Response { get; set; }
+			Dictionary<string, string> Headers { get; set; }
+			Stream Content { get; set; } // please set content type
+			void Transmit(); // only call once, subsequent calls ignored
+			NetworkStream DisownSocket(); // take control of the NetworkSocket, used for upgrades
+		}
+	}
+}
